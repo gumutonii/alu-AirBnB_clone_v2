2edc896b257863282b305cb84334ee7c5d4cc02d
Samenergy <sdushime38@gmail.com>
2024-02-15 10:30:13 -0800

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
+# Fabfile to generate a .tgz archive from the contents of web_static.
 import os.path
 from datetime import datetime
 from fabric.api import local
@@ -8,15 +8,22 @@ from fabric.api import local
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
+    file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                             dt.month,
+                                                             dt.day,
+                                                             dt.hour,
+                                                             dt.minute,
+                                                             dt.second)
+    # Create the 'versions' directory if it doesn't exist
+    if not os.path.isdir("versions"):
+        if local("mkdir -p versions").failed:
+            print("Failed to create directory 'versions'")
             return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+
+    # Create the .tgz archive
+    if local("tar -cvzf {} web_static".format(file_name)).failed:
+        print("Failed to create archive")
         return None
-    return file
+
+    return file_name
+
